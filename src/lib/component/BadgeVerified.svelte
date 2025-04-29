4ca9c35461d77a2e0592c9ea879db28cc99b9832
Josh <43449531+joshbker@users.noreply.github.com>
2025-04-29 02:30:23 +0100

    fix: correct size calculation for Check icon in BadgeVerified component

--- src/lib/component/BadgeVerified.svelte
+++ src/lib/component/BadgeVerified.svelte
@@ -2,11 +2,12 @@
     import { Badge, Check } from "lucide-svelte"
 
     export let size = 6
+    let checkSize = size / 2
 </script>
 
 <div class="relative">
     <div class="absolute top-0 left-0 w-full h-full flex items-center justify-center">
-        <Check class="h-{size/2} w-{size/2} text-white" strokeWidth={3.5} />
+        <Check class="h-{checkSize} w-{checkSize} text-white" strokeWidth={3.5} />
     </div>
     <Badge class="h-{size} w-{size} fill-primary text-primary" />
 </div>
