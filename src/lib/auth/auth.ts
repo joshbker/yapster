f8c83bd2fab81603ba6af159dee12e1c37791c5e
Josh <43449531+joshbker@users.noreply.github.com>
2025-04-29 01:54:57 +0100

    feat: enhance authentication configuration by adding additional fields and rate limiting rules

--- src/lib/auth/auth.ts
+++ src/lib/auth/auth.ts
@@ -18,11 +18,44 @@ export const auth = betterAuth({
 			enabled: true,
 		},
 		additionalFields: {
-			
+			banner: {
+				type: "string",
+				required: true,
+				defaultValue: ""
+			},
+			pronouns: {
+				type: "string",
+				required: true,
+				defaultValue: ""
+			},
+			bio: {
+				type: "string",
+				required: true,
+				defaultValue: ""
+			},
+			verified: {
+				type: "boolean",
+				required: true,
+				defaultValue: false
+			}
 		},
 	},
 	database: mongodbAdapter(database),
 	advanced: {
 		cookiePrefix: "yapster",
-	}
+	},
+	rateLimit: {
+		window: 30,
+		max: 100,
+		customRules: {
+			"/api/me/avatar": {
+				window: 60,
+				max: 3,
+			},
+			"/api/me/banner": {
+				window: 60,
+				max: 3,
+			},
+		}
+	},
 });
