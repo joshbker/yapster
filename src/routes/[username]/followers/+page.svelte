2cd47a8cdef1e9d46f8149aedb566d34ebd93792
Josh <43449531+joshbker@users.noreply.github.com>
2025-04-30 22:37:24 +0100

    refactor: remove back navigation function and update HeaderNavigation component to display user-specific titles in followers and following pages for improved clarity

--- src/routes/[username]/followers/+page.svelte
+++ src/routes/[username]/followers/+page.svelte
@@ -58,10 +58,6 @@
         }
     }
 
-    function handleBack() {
-        history.back();
-    }
-
     function handleTabChange(value) {
         if (value === "following") {
             goto(`/@${user.username}/following`, { replaceState: true });
@@ -71,7 +67,7 @@
 
 <div class="h-full w-full flex flex-col">
     <div class="m-0 p-0">
-        <HeaderNavigation back={handleBack} />
+        <HeaderNavigation name={`${user.name ?? user.username}'s Followers`} />
     </div>
     <div class="flex-1 flex flex-col px-2 max-w-xl self-center w-full">
         <div class="w-full">
--- src/routes/[username]/following/+page.svelte
+++ src/routes/[username]/following/+page.svelte
@@ -59,10 +59,6 @@
         }
     }
 
-    function handleBack() {
-        history.back();
-    }
-
     function handleTabChange(value) {
         if (value === "followers") {
             goto(`/@${user.username}/followers`, { replaceState: true });
@@ -72,7 +68,7 @@
 
 <div class="h-full w-full flex flex-col">
     <div class="m-0 p-0">
-        <HeaderNavigation back={handleBack} />
+        <HeaderNavigation  name={`${user.name ?? user.username}'s Following`} />
     </div>
     <div class="flex-1 flex flex-col px-2 max-w-xl self-center w-full">
         <div class="w-full">
