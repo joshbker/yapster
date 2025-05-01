5124b09d1dc982e36c55e585b251ce6a34436331
Josh <43449531+joshbker@users.noreply.github.com>
2025-05-01 12:32:59 +0100

    refactor: update ProfileActions component to improve event handling by preventing default actions and stopping propagation for dropdown menu items

--- src/lib/component/profile/ProfileActions.svelte
+++ src/lib/component/profile/ProfileActions.svelte
@@ -7,30 +7,38 @@
     export let viewer;
 </script>
 
-<DropdownMenu>
-    <DropdownMenuTrigger as-child>
-        <Button variant="outline" class="px-2 h-8">
-            <MoreHorizontal class="h-4 w-4" />
-        </Button>
-    </DropdownMenuTrigger>
-    <DropdownMenuContent>
-        <DropdownMenuItem class="gap-2 !text-destructive" on:click={() => {
-            alert("Report");
-        }}>
-            <Flag class="h-4 w-4" />
-            <p>Report</p>
-        </DropdownMenuItem>
-        <DropdownMenuItem class="gap-2 !text-destructive" on:click={() => {
-            alert("Block");
-        }}>
-            <Ban class="h-4 w-4" />
-            <p>Block</p>
-        </DropdownMenuItem>
-        <DropdownMenuItem class="gap-2" on:click={() => {
-            alert("Ignore");
-        }}>
-            <BellOff class="h-4 w-4" />
-            <p>Ignore</p>
-        </DropdownMenuItem>
-    </DropdownMenuContent>
-</DropdownMenu>
+<button on:click|stopPropagation|preventDefault>
+    <DropdownMenu>
+        <DropdownMenuTrigger as-child>
+            <Button variant="outline" class="px-2 h-8">
+                <MoreHorizontal class="h-4 w-4" />
+            </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+            <DropdownMenuItem class="gap-2 !text-destructive" on:click={(e) => {
+                e.preventDefault();
+                e.stopPropagation();
+                alert("Report");
+            }}>
+                <Flag class="h-4 w-4" />
+                <p>Report</p>
+            </DropdownMenuItem>
+            <DropdownMenuItem class="gap-2 !text-destructive" on:click={(e) => {
+                e.preventDefault();
+                e.stopPropagation();
+                alert("Block");
+            }}>
+                <Ban class="h-4 w-4" />
+                <p>Block</p>
+            </DropdownMenuItem>
+            <DropdownMenuItem class="gap-2" on:click={(e) => {
+                e.preventDefault();
+                e.stopPropagation();
+                alert("Ignore");
+            }}>
+                <BellOff class="h-4 w-4" />
+                <p>Ignore</p>
+            </DropdownMenuItem>
+        </DropdownMenuContent>
+    </DropdownMenu>
+</button>
