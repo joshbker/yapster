bd8d8b2daaf5d6d2d0a97038e9eef7ab9cd27aae
Josh <43449531+joshbker@users.noreply.github.com>
2025-05-01 18:45:33 +0100

    fix: remove patch for adding field

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,17 +1,5 @@
-import { user as User } from "$lib/data/model/user.js"
-
 export async function load({ locals }) {
     console.log("LOCALS", locals)
-
-    if (locals.user) {
-        const user = await User.findById(locals.user.id)
-        if (!user.likedComments) {
-            user.likedComments = []
-        }
-        await user.save()
-
-        locals.user.likedComments = user.likedComments
-    }
 
     return {
         user: locals.user
