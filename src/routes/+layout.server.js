ba8a46d44825b30d6f0ad45362d58f71dcdd8243
Josh <43449531+joshbker@users.noreply.github.com>
2025-05-01 18:46:15 +0100

    a

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,6 +1,17 @@
 export async function load({ locals }) {
     console.log("LOCALS", locals)
 
+    // Update all users to ensure they have likedComments field
+    // try {
+    //     // Use updateMany to set likedComments only where it doesn't exist
+    //     await User.updateMany(
+    //         { likedComments: { $exists: false } },
+    //         { $set: { likedComments: [] } }
+    //     );
+    // } catch (err) {
+    //     console.error("Error updating users with likedComments field:", err);
+    // }
+
     return {
         user: locals.user
     };
